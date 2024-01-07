1a2f3080b481bcd30e831f6074fdd3c1bfd6609f
erdemmgunal <65365648+erdemmgunal@users.noreply.github.com>
2024-01-07 13:18:18 +0300

    Initial commit

---
+++ src/data/resume-data.tsx
@@ -0,0 +1,167 @@
+import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
+
+export const RESUME_DATA = {
+  name: "<Hakki />",
+  initials: "HAKKI",
+  location: "Istanbul, Turkey",
+  locationLink: "https://www.google.com/maps/place/Istanbul/",
+  about:
+    "Hello my name is Hakkı Erdem Günal. I'm 19 years old guy (who like to automate life and make things easier)",
+  summary:
+    "I am Erdem Günal, a physics student at Marmara University. Building Lego and solving problems has become not only a hobby but also a passion for me. I have 3 years of software experience and I am proficient in technologies such as Python, Javascript, Typescript, React, Next.js. I am passionate about automation and productivity improvement. At the same time, I aim to develop effective web applications by combining my interest in design tools and my visual design skills with my technical knowledge. Playing basketball helps me learn teamwork while building physical and mental endurance. I love building good relationships with people and understanding different perspectives. I stand out with my skills in being successful in projects that require cooperation and teamwork.",
+  avatarUrl: "https://avatars.githubusercontent.com/u/65365648?v=4",
+  personalWebsiteUrl: "",
+  contact: {
+    email: "erdemmgunal@gmail.com",
+    tel: "+905320664332",
+    social: [
+      {
+        name: "GitHub",
+        url: "https://github.com/erdemmgunal",
+        icon: GitHubIcon,
+      },
+      {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/hakkierdem/",
+        icon: LinkedInIcon,
+      },
+      {
+        name: "Twitter",
+        url: "https://twitter.com/erdemmgunal/",
+        icon: XIcon,
+      }
+    ],
+  },
+  education: [
+    {
+      school: "Marmara University",
+      degree: "Bachelor's Degree in Physics",
+      start: "2023",
+      end: "Present",
+    },
+    {
+      school: "Istanbul Kadıkoy High School",
+      degree: "",
+      start: "2019",
+      end: "2023",
+    },
+    {
+      school: "TED Rönesans College",
+      degree: "",
+      start: "2018",
+      end: "2019",
+    },
+  ],
+  work: [
+      {
+      company: "Monitorist",
+      link: "https://monitorist.vercel.app/",
+      badges: ["Owner"],
+      title: "",
+      start: "2021",
+      end: "Present",
+      description:
+        "As the founder and primary operator of a cutting-edge platform within a Cookgroup, I have played a pivotal role in the development and execution of various responsibilities. The platform operates a sophisticated monitoring system, overseeing stock changes across more than 20 websites on a 24/7 basis.",
+    },
+  ],
+  certs: [
+    {
+      title: "0'dan ileri seviye'ye dijital pazarlama egitimi",
+      link:'',
+      start: "2024",
+      end: "",
+      description:"Ridvan Aydin - Marmara University Sinerji Club",
+    }
+  ],
+  skills: [
+    "Python",
+    "JavaScript",
+    "TypeScript",
+    "React/Next.js",
+    "Photoshop",
+    "Problem Solving",
+    "System Automation",
+    "Basketball",
+  ],
+  projects: [
+    {
+      title: "Monitorist",
+      techStack: [
+        "Python",
+        "Javascript",
+        "Next.js",
+        "GraphQL",
+      ],
+      description: "A platform to build and grow your business",
+      link: {
+        label: "Monitorist",
+        href: "https://monitorist.vercel.app/",
+      },
+    },
+    {
+      title: "Idata Scraper",
+      techStack: ["Python"],
+      description:
+        "It makes an appointment for you that is difficult to make through the site. (Education purposes only)",
+      link: {
+        label: "idata scraper",
+        href: "https://github.com/erdemmgunal/idata-scraper/",
+      },
+    },
+    {
+      title: "Troy Monitor",
+      techStack: ["Python", "SQL"],
+      description:"The program notice you when an iPhone 15 availabile on the site.",
+      link: {
+        label: "troy monitor",
+        href: "https://github.com/erdemmgunal/troy-monitor",
+      },
+    },
+    {
+      title: "Macmillan Scraper",
+      techStack: ["Python"],
+      description:"Program that extracts the PDF of the book whose PDF download access is not allowed. (Education purposes only)",
+      link: {
+        label: "macmillan scraper",
+        href: "https://github.com/erdemmgunal/macmillan-education",
+      },
+    },
+    {
+      title: "Marti Client",
+      techStack: ["Python"],
+      description:"You can monitor and ring all MARTI company scooters, motorcycles and mopeds around you from a single screen. (Education purposes only)",
+      link: {
+        label: "marti client",
+        href: "https://github.com/erdemmgunal/marti-scooter",
+      },
+    },
+    {
+      title: "BMW - MERCEDES Monitor",
+      techStack: ["Python", "SQL"],
+      description:"The program through which you can buy a BMW or MERCEDES at store price without going to the black market, through their website.",
+      link: {
+        label: "bmw mercedes",
+        href: "https://github.com/erdemmgunal/bmw-mercedes-monitor",
+      },
+    },
+    {
+      title: "Hakki.info",
+      techStack: ["Typescript", "Javascript", "CSS"],
+      description:"Hakki.info is a personal resume website built using a technology stack that includes React, Next.js, Typescript, and CSS.",
+      link: {
+        label: "hakki info resume",
+        href: "https://github.com/erdemmgunal/hakkierdem-resume",
+      },
+    }
+  ],
+  languages: [
+    {
+      name: "English",
+      level: "Full professional competence",
+    },
+    {
+      name: "Turkish",
+      level: "Native or second language proficiency",
+    }
+  ]
+} as const;
